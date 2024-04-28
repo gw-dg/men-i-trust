1d7ad8030ff339de807704e3f2cab404754de3df
Bhaskar Jha <92924837+gw-dg@users.noreply.github.com>
2024-04-28 15:40:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,10 +2,14 @@
 let countEl = document.getElementById("count-el")
 let count = 0
 
+if(localStorage.getItem("count")) {
+    count = JSON.parse(localStorage.getItem("count"));
+}
+
 function increment() {
-    count += 1
-    localStorage.setItem("count",json.stringify(count))
-    countEl.textContent = count + " Times hearted ❤️"
+    count += 1;
+    localStorage.setItem("count", JSON.stringify(count));
+    countEl.textContent = count + " Times hearted ❤️";
 }
 
 /*function save() {
